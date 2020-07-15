543bdcdaa9322928b52b47b4b8dae48a0efe8f94
vincentjava <vincentjava@yahoo.com.tw>
2020-07-15 14:03:22 +0800

    Python

--- d02/PrintDemo.py
+++ d02/PrintDemo.py
@@ -2,7 +2,7 @@ import random
 symbol = '2330.TW'
 name = '台積電'
 price = 363.5  # 現在價格
-cost = 350.5  # 成本價
+cost = 370.5  # 成本價
 shares = 3000  # 股數
 
 print("2330.TW 台積電 成本: 350.5 張數: 3 張 成交價: 363.5")
