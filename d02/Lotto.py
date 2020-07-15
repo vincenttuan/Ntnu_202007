8fa46f475b66dcdd839a9e47c9f83cf810f9672d
vincentjava <vincentjava@yahoo.com.tw>
2020-07-15 13:38:07 +0800

    Python

---
+++ d02/Lotto.py
@@ -0,0 +1,11 @@
+# 電腦選號四星彩 0000~9999
+import random
+
+num = random.randint(1, 9999)
+print("四星彩電腦選號:", num)
+
+n1 = random.randint(0, 9)
+n2 = random.randint(0, 9)
+n3 = random.randint(0, 9)
+n4 = random.randint(0, 9)
+print("四星彩電腦選號:", n1, n2, n3, n4)
