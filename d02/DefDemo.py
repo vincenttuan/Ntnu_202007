ecd3d3986cdf23b0612127af794e6bb6a716e078
vincentjava <vincentjava@yahoo.com.tw>
2020-07-15 14:39:52 +0800

    Python

---
+++ d02/DefDemo.py
@@ -0,0 +1,11 @@
+# 請幫我印出 Hello Python
+def printHello():  # 此方法為無回傳值方法
+    print("Hello Python")
+
+def sum(x, y): # 此方法為有回傳值方法
+    result = x + y
+    return result # 回傳值
+
+printHello()
+result = sum(10, 20)
+print(result)
