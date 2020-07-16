17e0930df84f705b081a6db8f21594ebe869a85a
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 13:59:11 +0800

    Python

--- d03/ForInDemo4.py
+++ d03/ForInDemo4.py
@@ -1,6 +1,12 @@
-emps = {"Tom": 70000, "John": 100000, "Mary": 90000}
+emps = {"Tom": 70000, "John": 100000, "Jane": 30000, "Mary": 90000}
 # 薪資最低
-
+name, min = '', -1
+for key in emps:
+    salary = emps[key]
+    if salary < min or min == -1:
+        name = key
+        min = salary
+print(name, min)
 # 薪資最高
 name, max = '', 0
 for key in emps:
