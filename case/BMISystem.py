56203dccffef8dcb822e071f56a581cda9f5028e
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 09:29:03 +0800

    Python

--- case/BMISystem.py
+++ case/BMISystem.py
@@ -18,7 +18,7 @@ def choice():
     if id == 1:
         h = float(input('請輸入身高:'))
         w = float(input('請輸入體重:'))
-        bmi = w / ((h / 100) ** 2)
+        bmi = getBMI(h, w)
         print("%.2f" % bmi)
         print('按下任意鍵繼續...')
         sys.stdin.read(1)
@@ -29,6 +29,9 @@ def choice():
         print("輸入錯誤請重新輸入")
         menu()
 
+def getBMI(h, w):
+    bmi = w / ((h / 100) ** 2)
+    return bmi
 
 # 主程式
 if __name__ == '__main__':
