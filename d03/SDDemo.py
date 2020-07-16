3421c617f26755f2980b923e1ddc1ca5bc539526
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 11:41:11 +0800

    Python

--- d03/SDDemo.py
+++ d03/SDDemo.py
@@ -1,3 +1,4 @@
+import math
 def getSum(scores):
     sum = 0
     for score in scores:
@@ -8,9 +9,15 @@ def getAvg(scores):
     avg = getSum(scores) / len(scores)
     return avg
 
-def getSD():
-    pass
+def getSD(scores):
+    sum = 0
+    avg = getAvg(scores)
+    for score in scores:
+        sum += math.pow(score - avg, 2)
+    sd = math.sqrt(sum / len(scores))
+    return sd
 
 scores1 = [40, 70, 100]
 scores2 = [10, 15, 20]
 
+print(getSD(scores1), getSD(scores2))
