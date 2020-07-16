71bc1bd634c36be5c90d7434da4cb3cbd2c271a4
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 15:20:42 +0800

    Python

--- d03/PokerDemo.py
+++ d03/PokerDemo.py
@@ -1,3 +1,4 @@
+import random as r
 poker = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 print(poker)
 # 反轉
@@ -7,3 +8,10 @@ print(poker)
 poker.sort()
 print(poker)
 # 洗牌(Shuffle)
+
+n1, n2 = r.randint(0, len(poker)-1), r.randint(0, len(poker)-1)
+p1 = poker[n1]
+poker[n1] = poker[n2]
+poker[n2] = p1
+print(poker)
+#for i in range(0, 10000):
