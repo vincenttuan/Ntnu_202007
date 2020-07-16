126fe2da979bd7013d6b592e7b7e41005391d87e
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 13:43:59 +0800

    Python

--- d03/ForInDemo4.py
+++ d03/ForInDemo4.py
@@ -2,3 +2,10 @@ emps = {"Tom": 70000, "John": 100000, "Mary": 90000}
 print(emps['Tom'])
 for key in emps:
     print(key, emps[key])
+
+# 平均薪資
+sum = 0
+for key in emps:
+    salary = emps[key]
+    sum += salary
+print(sum, sum/len(emps))
