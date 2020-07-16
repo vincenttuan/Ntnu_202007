734bf165fd02edba1dfdb6fd44d73a0197358291
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 09:14:45 +0800

    Python

---
+++ case/BMISystem.py
@@ -0,0 +1,27 @@
+'''
+使用者可選擇是否要輸入身高,體重等資訊
+使用者可以離開系統
+系統可以重複使用
+'''
+# 系統目錄
+def menu():
+    print("BMI 計算系統")
+    print("--------------")
+    print("1. 輸入資料")
+    print("2. Exit")
+    print("--------------")
+    id = int(input('請輸入選項:'))
+    if id == 1:
+        h = float(input('請輸入身高:'))
+        w = float(input('請輸入體重:'))
+        bmi = w / ((h/100)**2)
+        print("%.2f", bmi)
+        menu()
+    elif id == 2:
+        print("謝謝使用")
+    else:
+        print("輸入錯誤請重新輸入")
+        menu()
+# 主程式
+if __name__ == '__main__':
+    menu()
