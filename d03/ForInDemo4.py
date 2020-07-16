97d20b2d664b80c042a78d9fd5256080f54ba7ce
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 13:52:29 +0800

    Python

--- d03/ForInDemo4.py
+++ d03/ForInDemo4.py
@@ -1,4 +1,15 @@
 emps = {"Tom": 70000, "John": 100000, "Mary": 90000}
+
+# 薪資最高
+name, max = '', 0
+for key in emps:
+    salary = emps[key]
+    if salary > max:
+        name = key
+        max = salary
+print(name, max)
+
+
 print(emps['Tom'])
 for key in emps:
     print(key, emps[key])
@@ -9,3 +20,5 @@ for key in emps:
     salary = emps[key]
     sum += salary
 print(sum, sum/len(emps))
+
+
