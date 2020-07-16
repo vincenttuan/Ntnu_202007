c00886a512f647b14de526919e47203e67478a9f
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 13:00:39 +0800

    Python

---
+++ case/Utility.py
@@ -0,0 +1,28 @@
+import math
+'''
+工具資源提供說明:
+1.getSum(items)  取得總和
+2.getAvg(items)  取得平均
+3.getSD(items)   取得標準差
+4.getCV(sd, avg) 取得變異係數
+'''
+def getSum(items):
+    sum = 0
+    for item in items:
+        sum += item
+    return sum
+
+def getAvg(items):
+    avg = getSum(items) / len(items)
+    return avg
+
+def getSD(items):
+    sum = 0
+    avg = getAvg(items)
+    for item in items:
+        sum += math.pow(item - avg, 2)
+    sd = math.sqrt(sum / len(items))
+    return sd
+
+def getCV(sd, avg):
+    return sd / avg
