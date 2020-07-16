bc61b5aeb57235d591ca61df87ddc713e5173a14
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 10:24:37 +0800

    Python

---
+++ d03/WhileGame.py
@@ -0,0 +1,12 @@
+ans = 72
+min, max = 0, 100
+while True:
+    guess = int(input('請輸入數字 %d ~ %d : ' % (min, max)))
+    if(guess < ans):
+        min = guess
+    elif(guess > ans):
+        max = guess
+    else:
+        print("恭喜答對了")
+        break;
+
