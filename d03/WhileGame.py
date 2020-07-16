414836e51be0d052e7d71da9c6fd289a0ac96269
vincentjava <vincentjava@yahoo.com.tw>
2020-07-16 10:42:54 +0800

    Python

--- d03/WhileGame.py
+++ d03/WhileGame.py
@@ -3,12 +3,12 @@ ans = r.randint(1, 99)
 min, max = 0, 100
 while True:
     guess = int(input('請輸入數字 %d ~ %d : ' % (min, max)))
-    # 確認所猜的數字是否合理 ?
+    # 1.確認所猜的數字是否合理 ?
     if not (guess > min and guess < max):
         print("輸入錯誤, 請重新輸入")
         continue
 
-    # 確認是否有答對 ?
+    # 2.確認是否有答對 ?
     if(guess < ans):
         min = guess
     elif(guess > ans):
