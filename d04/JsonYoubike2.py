4c89806475ccc1c6de249625ad98c5a352f6d8e2
vincentjava <vincentjava@yahoo.com.tw>
2020-07-17 13:24:07 +0800

    Python

--- d04/JsonYoubike2.py
+++ d04/JsonYoubike2.py
@@ -7,17 +7,18 @@ data = json.loads(resp.text)
 retVal = data['retVal']
 sbi = 10
 bemp = 10
+max_d = 500
 for i in range(1, 405):
     sno = '%04d' % i
     try:
         youbike = retVal[sno]
-        if(int(youbike['sbi']) >= sbi and int(youbike['bemp']) >= bemp):
-            # 計算距離
-            # 站台的經緯度
-            lat = float(youbike['lat'])
-            lng = float(youbike['lng'])
-            d = gcd.cal_dis_meters(25.027951, 121.528596, lat, lng)
-            print(d, youbike)
+        # 計算距離
+        # 站台的經緯度
+        lat = float(youbike['lat'])
+        lng = float(youbike['lng'])
+        d = gcd.cal_dis_meters(25.027951, 121.528596, lat, lng)
+        if(int(youbike['sbi']) >= sbi and int(youbike['bemp']) >= bemp and d <= max_d):
+            print('%.2fm' % d, youbike)
     except:
         #print('無此站台', sno)
         pass
