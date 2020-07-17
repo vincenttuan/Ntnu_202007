c886d53bc7109366c6553a984b36097475106a52
vincentjava <vincentjava@yahoo.com.tw>
2020-07-17 10:56:14 +0800

    Python

--- d04/JsonDemo3.py
+++ d04/JsonDemo3.py
@@ -1,5 +1,11 @@
 import requests
+import json
 
 path = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
 resp = requests.get(path)
-print(resp.text)
+print(resp)
+list = json.loads(resp.text)
+#print(list)
+for data in list:
+    if data['品名'].__contains__("日本"):
+        print(data['品名'], data['不合格原因'])
