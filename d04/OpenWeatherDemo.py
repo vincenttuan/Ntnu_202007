18fc14c22bb669907eadb3c393f6550934375e8c
vincentjava <vincentjava@yahoo.com.tw>
2020-07-17 14:04:40 +0800

    Python

--- d04/OpenWeatherDemo.py
+++ d04/OpenWeatherDemo.py
@@ -1,10 +1,15 @@
 import requests
+import json
 
 def getWeather(city):
     apikey = open('api.key', 'r').read()
     path = 'https://api.openweathermap.org/data/2.5/find?q=%s&appid=%s'
     resp = requests.get(path % (city, apikey))
-    print(resp.text)
+    # 分析 json
+    data = json.loads(resp.text)
+    main = data['list'][0]['main']
+    return main['temp'], main['feels_like'], main['humidity']
 
 if '__main__' == __name__ :
-    getWeather('Taipei')
+    temp, feels_like, humidity = getWeather('Taipei')
+    print("溫度: %.2f 體感: %.2f 濕度: %d %%" % (temp, feels_like, humidity))
