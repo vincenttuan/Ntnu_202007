c5dad004d5b882f8257c6efb9640ed9512227886
vincentjava <vincentjava@yahoo.com.tw>
2020-07-17 10:34:31 +0800

    Python

--- d04/JsonDemo2.py
+++ d04/JsonDemo2.py
@@ -3,3 +3,12 @@ data = '[{"name":"John", "age":28, "salary":60000},{"name":"Mary", "age":25, "sa
 user = json.loads(data)  # 轉成 Python 的 list 格式
 print(data, type(data))
 print(user, type(user))
+
+for u in user:
+    print(u, type(u))
+
+for u in user:
+    if u['name'] == 'Mary':
+        print(u['salary'])
+
+print([u['salary'] for u in user if u['name'] == 'Mary'])
