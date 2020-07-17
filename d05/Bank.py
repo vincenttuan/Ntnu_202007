8f92232a01b67f9a8fc71effde9267ad28f7bbb4
vincentjava <vincentjava@yahoo.com.tw>
2020-07-17 15:46:33 +0800

    Python

--- d05/Bank.py
+++ d05/Bank.py
@@ -10,3 +10,30 @@ class Account:
 
     def printAct(self):  # 查詢餘額
         print("%s 帳戶餘額: %d" % (self.actName, self.balnce))
+
+def menu(act):
+    print("-----------")
+    print("1.存款")
+    print("2.提款")
+    print("3.查詢")
+    print("9.離開")
+    print("-----------")
+    id = int(input('請選擇:'))
+    if id == 1:
+        money = int(input('請輸入存款金額:'))
+        act.save(money)
+    elif id == 2:
+        money = int(input('請輸入提款金額:'))
+        act.withdraw(money)
+    elif id == 3:
+        act.printAct()
+    elif id == 9:
+        return
+    menu(act)
+
+if __name__ == '__main__':
+    actName = input('請輸入戶名:')
+    act = Account()
+    act.actName = actName
+    menu(act)
+
