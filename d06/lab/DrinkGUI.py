e630e7fc36a15f99fbcbc720423792925dc9c85c
vincentjava <vincentjava@yahoo.com.tw>
2020-07-20 15:50:03 +0800

    Python

--- d06/lab/DrinkGUI.py
+++ d06/lab/DrinkGUI.py
@@ -2,21 +2,25 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 
-def open_img(x):
+# 顯示圖片
+def open_img(x):  # x -> 檔名
     img = Image.open(x + '.png')
     img = img.resize((150, 250), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(img)
-    panel = tk.Label(app, image=img)
-    panel.image = img
+    img = ImageTk.PhotoImage(img)  # 當檔案轉成影像物件
+    panel = tk.Label(app, image=img)  # 將影像物件放入到 Label
+    panel.image = img  # 將影像物件放入到 Label
     panel.grid(column=0, row=6, padx=70, pady=10)
 
+# 按下結帳
 def submit():
     bag = lambda m: '要' if m else '不要'
-    extra = lambda m: 1*nums.get() if m else 0 # 手提袋費用
+    extra = lambda m: 1*nums.get() if m else 0  # 手提袋費用
     print(nums.get(), comboExample.get(), radioValue.get(), chkValue.get())
-    labelValue.set("數量：{0}\n品名：{1}\n單價：{2}\n糖分：{3}\n提袋：{4}\n小計：{5}".format(nums.get(), comboExample.get(), drinks[comboExample.get()], sugar[radioValue.get()], bag(chkValue.get()), nums.get() * drinks[comboExample.get()] + extra(chkValue.get())))
+    labelValue.set("數量：{0}\n品名：{1}\n單價：{2}\n糖分：{3}\n提袋：{4}\n小計：{5}"
+                   .format(nums.get(), comboExample.get(), drinks[comboExample.get()], sugar[radioValue.get()], bag(chkValue.get()), nums.get() * drinks[comboExample.get()] + extra(chkValue.get())))
     open_img(comboExample.get())
 
+# GUI 布局
 app = tk.Tk()
 app.geometry('400x500')
 
@@ -30,9 +34,8 @@ nums.set(1)
 entry = tk.Entry(app, width=20, textvariable=nums)
 entry.grid(column=0, row=1, padx=50, sticky="W")
 
-drinks = {'珍奶': 50, '綠茶': 30, '芒果汁': 70, '西瓜汁': 25}
-
 tk.Label(app, text="品項:").grid(column=0, row=2, padx=10, sticky="W")
+drinks = {'珍奶': 50, '綠茶': 30, '芒果汁': 70, '西瓜汁': 25}
 comboExample = ttk.Combobox(app,
                             values=list(drinks.keys()),
                             state="readonly")  # or disabled
@@ -41,7 +44,6 @@ comboExample.grid(column=0, row=2, padx=50, sticky="W")
 comboExample.current(0)
 
 radioValue = tk.StringVar()
-
 sugar = {'A': '正常', 'B': '少糖', 'C': '半糖', 'D': '無糖'}
 deltaX = 10
 for k, v in sugar.items():
