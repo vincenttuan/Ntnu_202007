663554416bc706a2efa7fea8643f0670cc2014ea
vincentjava <vincentjava@yahoo.com.tw>
2020-07-20 13:21:59 +0800

    Python

--- d06/GUI.py
+++ d06/GUI.py
@@ -4,4 +4,13 @@ win = tkinter.Tk()
 win.title('我的小視窗')
 win.geometry("300x400")
 
+label = tkinter.Label(win, text="Hello")
+label.pack()
+
+button1 = tkinter.Button(win, text="OK")
+button1.pack(side=tkinter.LEFT)
+
+button2 = tkinter.Button(win, text="Cancel")
+button2.pack(side=tkinter.RIGHT)
+
 win.mainloop()
