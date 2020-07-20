c71d45534d3ffd00885af001cfba4e2eecc7b994
vincentjava <vincentjava@yahoo.com.tw>
2020-07-20 14:45:35 +0800

    Python

--- d06/LoginGUI.py
+++ d06/LoginGUI.py
@@ -1,14 +1,22 @@
 import tkinter
 
-win = tkinter.TK()
+win = tkinter.Tk()
 win.title('登入畫面')
-win.geometry("300x400")
+win.geometry("500x300")
 
 label   = tkinter.Label(win, text="帳號", font=('Arial', 20))
 label2  = tkinter.Label(win, text="密碼", font=('Arial', 20))
-entry   = tkinter.Entry(win)
-entry2  = tkinter.Entry(win)
-button  = tkinter.Button(win, text="登入")
-button2 = tkinter.Button(win, text="取消")
+entry   = tkinter.Entry(win, font=('Arial', 20))
+entry2  = tkinter.Entry(win, font=('Arial', 20))
+button  = tkinter.Button(win, text="登入", font=('Arial', 20))
+button2 = tkinter.Button(win, text="取消", font=('Arial', 20))
+
+# 登入配置
+label.grid(row=0, column=0)
+entry.grid(row=0, column=1)
+label2.grid(row=1, column=0)
+entry2.grid(row=1, column=1)
+button.grid(row=2, column=0)
+button2.grid(row=2, column=1)
 
 win.mainloop()
