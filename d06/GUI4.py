4bc7e31d71f689df939d38a7f16881b5243eb648
vincentjava <vincentjava@yahoo.com.tw>
2020-07-20 14:18:35 +0800

    Python

---
+++ d06/GUI4.py
@@ -0,0 +1,19 @@
+import tkinter
+import tkinter.messagebox
+def show_info():
+    r = tkinter.messagebox.showinfo('對話框', 'This is [info] dialog')
+    print(r) # 按下Ok --> ok
+def show_askokcancel():
+    r = tkinter.messagebox.askokcancel('對話框', 'This is [okcancel] dialog')
+    print(r) # 按下ok --> True, cancel --> False
+def show_askyesno():
+    r = tkinter.messagebox.askyesno('對話框', 'This is [yesno] dialog')
+    print(r) # 按下yes --> True, no --> False
+def show_askyesnocancel():
+    r = tkinter.messagebox.askyesnocancel('對話框', 'This is [yesnocancel] dialog')
+    print(r) # 按下yes --> True, no --> False, cancel --> None
+win = tkinter.Tk()
+win.geometry("300x400")
+button1 = tkinter.Button(win, text="Click me", command=show_info)
+button1.pack()
+win.mainloop()
