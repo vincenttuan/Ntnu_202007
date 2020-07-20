b563f6268c2d44f623ad4a02a64478a1da1d8bd1
vincentjava <vincentjava@yahoo.com.tw>
2020-07-20 13:57:53 +0800

    Python

--- d06/GUI3.py
+++ d06/GUI3.py
@@ -6,6 +6,10 @@ def msg():
     temp, feels_like, humidity, description = w.getWeather('taipei')
     mystr.set("氣溫: %.2f C\n體感: %.2f C\n濕度: %d %%\n%s" % (temp-273.15, feels_like-273.15, humidity, description))
 
+def msg2():
+    temp, feels_like, humidity, description = w.getWeather('taoyuan')
+    mystr.set("氣溫: %.2f C\n體感: %.2f C\n濕度: %d %%\n%s" % (temp-273.15, feels_like-273.15, humidity, description))
+
 def quit():
     win.quit()
 
@@ -18,8 +22,11 @@ label = tkinter.Label(win, textvariable=mystr, font=('Arial', 20))
 mystr.set("")
 label.pack()
 
-button1 = tkinter.Button(win, text="OK", command=msg, font=('Arial', 20))
+button1 = tkinter.Button(win, text="台北", command=msg, font=('Arial', 20))
 button1.pack(side=tkinter.LEFT)
+
+button3 = tkinter.Button(win, text="桃園", command=msg2, font=('Arial', 20))
+button3.pack(side=tkinter.LEFT)
 
 button2 = tkinter.Button(win, text="Cancel", command=quit, font=('Arial', 20))
 button2.pack(side=tkinter.RIGHT)
