9334f43ae5a505133f3dff2928b08cf5ce62f052
vincentjava <vincentjava@yahoo.com.tw>
2020-07-20 13:29:21 +0800

    Python

--- d06/GUI.py
+++ d06/GUI.py
@@ -1,16 +1,23 @@
 import tkinter
+from tkinter import messagebox
+
+def msg():
+    messagebox.showinfo('Info', 'Hello')
+
+def quit():
+    win.quit()
 
 win = tkinter.Tk()
 win.title('我的小視窗')
 win.geometry("300x400")
 
-label = tkinter.Label(win, text="Hello")
+label = tkinter.Label(win, text="Hello", font=('Arial', 20))
 label.pack()
 
-button1 = tkinter.Button(win, text="OK")
+button1 = tkinter.Button(win, text="OK", command=msg, font=('Arial', 20))
 button1.pack(side=tkinter.LEFT)
 
-button2 = tkinter.Button(win, text="Cancel")
+button2 = tkinter.Button(win, text="Cancel", command=quit, font=('Arial', 20))
 button2.pack(side=tkinter.RIGHT)
 
 win.mainloop()
