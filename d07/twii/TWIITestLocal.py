13d593c18fab33747f1b9bb2f6c4b4fcbf94a927
vincentjava <vincentjava@yahoo.com.tw>
2020-07-21 09:45:21 +0800

    Python

--- d07/twii/TWIITestLocal.py
+++ d07/twii/TWIITestLocal.py
@@ -2,4 +2,12 @@ import requests
 
 file = open('BWIBBU_d.csv', 'r')
 data = file.read()
-print(data)
+
+# 資料整理
+# "證券代號","證券名稱","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
+data = data.replace('"-"', '-1')
+data = data.replace('"', '')
+for d in data.split("\n"):
+    list = d.split(",")
+    if len(list) == 8:
+        print(d)
